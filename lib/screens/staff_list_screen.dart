558e83816a07e5e8f8aaef11333bb738b8b81ed8
david1707 <DavidAlacant@gmail.com>
2021-05-17 17:36:57 +0200

    StaffData created

--- lib/screens/staff_list_screen.dart
+++ lib/screens/staff_list_screen.dart
@@ -1,42 +1,14 @@
 import 'package:flutter/material.dart';
 
-import '../models/staff.dart';
 import '../widgets/staff_card.dart';
+import '../models/staff_data.dart';
+import '../models/staff.dart';
 
 class StaffListScreen extends StatelessWidget {
   static const String routeName = '/';
 
-  List<Staff> staffList = [
-    Staff(
-      name: 'David',
-      surnames: 'Membrives',
-      photoUrl:
-          'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.abdn.ac.uk%2Fnews%2Fimages%2Fmasc_average_male_rdax_450x600.jpg&f=1&nofb=1',
-      role: Roles.CEO,
-    ),
-    Staff(
-      name: 'Diana',
-      surnames: 'Belliure',
-      photoUrl:
-          'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.robnicholls.co.uk%2Ffiles%2Faverage-yamahito.jpg&f=1&nofb=1',
-      role: Roles.Manager,
-    ),
-    Staff(
-      name: 'Han',
-      surnames: 'Yong',
-      photoUrl:
-          'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.abdn.ac.uk%2Fnews%2Fimages%2Fmasc_average_male_rdax_450x600.jpg&f=1&nofb=1',
-      role: Roles.Accountant,
-    ),
-    Staff(
-      name: 'Siyanda',
-      surnames: 'Nea',
-      photoUrl:
-          'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.robnicholls.co.uk%2Ffiles%2Faverage-yamahito.jpg&f=1&nofb=1',
-      role: Roles.Developer,
-    ),
-  ];
-
+  List<Staff> staffList = StaffData().staffList;
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
