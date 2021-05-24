8a852ce02bc5e4cb522ec2d2eebc3a3a5da77611
david1707 <DavidAlacant@gmail.com>
2021-05-24 13:28:06 +0200

    Showing TextFormFields on StaffInfo.dart and navigation back and forth

--- lib/screens/staff_info.dart
+++ lib/screens/staff_info.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
 
-class StaffInfoScreen extends StatelessWidget {
+import '../models/staff.dart';
 
+class StaffInfoScreen extends StatefulWidget {
   static const String routeName = '/staff-info';
 
   @override
+  _StaffInfoScreenState createState() => _StaffInfoScreenState();
+}
+
+class _StaffInfoScreenState extends State<StaffInfoScreen> {
+  @override
   Widget build(BuildContext context) {
+    final staff = ModalRoute.of(context).settings.arguments as Staff;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Staff Info'),
+        title: Text('Info ${staff.name} ${staff.surnames}'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.save,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // Update and, if confirmed, go back to the Staff List Screen
+              Navigator.pop(context);
+            },
+          )
+        ],
       ),
-      body: Center(
-        child: Text('Staff Info Screen'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 15),
+        child: Form(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Name'),
+                  initialValue: '${staff.name}',
+                  textInputAction: TextInputAction.next,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Surname(s)'),
+                  initialValue: '${staff.surnames}',
+                  textInputAction: TextInputAction.next,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Photo URL'),
+                  initialValue: '${staff.photoUrl}',
+                  textInputAction: TextInputAction.next,
+                ),
+                
+                
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/widgets/staff_card.dart
+++ lib/widgets/staff_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/staff.dart';
+import '../screens/staff_info.dart';
 
 class StaffCard extends StatelessWidget {
   final Staff staff;
@@ -10,44 +11,53 @@ class StaffCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: CircleAvatar(
-                minRadius: 30,
-                backgroundImage: NetworkImage(
-                  staff.photoUrl,
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            StaffInfoScreen.routeName,
+            arguments: staff,
+          );
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: CircleAvatar(
+                  minRadius: 30,
+                  backgroundImage: NetworkImage(
+                    staff.photoUrl,
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: SizedBox(),
-          ),
-          Expanded(
-            flex: 5,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 40),
-              child: Column(
-                children: [
-                  Text(
-                    '${staff.name} ${staff.surnames}',
-                  ),
-                  Text(
-                    '${staff.printRole()}',
-                    style: TextStyle(
-                        color: Colors.grey, fontStyle: FontStyle.italic),
-                  ),
-                ],
+            Expanded(
+              flex: 2,
+              child: SizedBox(),
+            ),
+            Expanded(
+              flex: 5,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 40),
+                child: Column(
+                  children: [
+                    Text(
+                      '${staff.name} ${staff.surnames}',
+                    ),
+                    Text(
+                      '${staff.printRole()}',
+                      style: TextStyle(
+                          color: Colors.grey, fontStyle: FontStyle.italic),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
